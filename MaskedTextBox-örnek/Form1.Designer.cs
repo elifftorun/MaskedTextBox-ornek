eb3f5eae5f14313467c2a830ebf9f325743201df
elfi <145452036+elifftorun@users.noreply.github.com>
2024-02-13 10:22:01 +0300

    Add files via upload

---
+++ MaskedTextBox-örnek/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace MaskedTextBox_örnek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.maskTC = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.maskTelefon = new System.Windows.Forms.MaskedTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.maskDTarih = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.maskKart = new System.Windows.Forms.MaskedTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maskIp = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // maskTC
+            // 
+            this.maskTC.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskTC.Location = new System.Drawing.Point(349, 79);
+            this.maskTC.Name = "maskTC";
+            this.maskTC.Size = new System.Drawing.Size(246, 38);
+            this.maskTC.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(205, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "T.C. No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(221, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Telefon:";
+            // 
+            // maskTelefon
+            // 
+            this.maskTelefon.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskTelefon.Location = new System.Drawing.Point(349, 131);
+            this.maskTelefon.Name = "maskTelefon";
+            this.maskTelefon.Size = new System.Drawing.Size(246, 38);
+            this.maskTelefon.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(145, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Doğum Tarihi:";
+            // 
+            // maskDTarih
+            // 
+            this.maskDTarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskDTarih.Location = new System.Drawing.Point(349, 182);
+            this.maskDTarih.Name = "maskDTarih";
+            this.maskDTarih.Size = new System.Drawing.Size(246, 38);
+            this.maskDTarih.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(134, 234);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(209, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kart Numarası:";
+            // 
+            // maskKart
+            // 
+            this.maskKart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskKart.Location = new System.Drawing.Point(349, 234);
+            this.maskKart.Name = "maskKart";
+            this.maskKart.Size = new System.Drawing.Size(246, 38);
+            this.maskKart.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(200, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(142, 31);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "IP Adresi:";
+            // 
+            // maskIp
+            // 
+            this.maskIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.maskIp.Location = new System.Drawing.Point(349, 288);
+            this.maskIp.Name = "maskIp";
+            this.maskIp.Size = new System.Drawing.Size(246, 38);
+            this.maskIp.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(806, 436);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.maskIp);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.maskKart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.maskDTarih);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.maskTelefon);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.maskTC);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox maskTC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MaskedTextBox maskTelefon;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.MaskedTextBox maskDTarih;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.MaskedTextBox maskKart;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox maskIp;
+    }
+}
+
